612687c724512515858f5614b2bb163aeb983ce4
sunw0o0 <sunwoo3908@naver.com>
2026-09-09 17:18:35 +0900

    Create kws_vector.hpp

---
+++ day_2/hw2/kws_vector.hpp
@@ -0,0 +1,35 @@
+#ifndef KSW_VECTOR_HPP
+#define KSW_VECTOR_HPP
+
+#include <string>
+#include "DataFrame.hpp"
+
+template <typename T>
+class ksw_vector : public dataframe<T>
+{
+protected:
+    using dataframe<T>::data_;
+    using dataframe<T>::size_;
+
+private:
+    size_t capacity_;
+
+public:
+    ksw_vector(size_t init_capacity = 10);
+
+    virtual size_t size() override;
+    virtual void clear() override;
+
+    void push_back(const T& element);
+    void pop_back();
+    void erase(size_t index);
+
+    T& operator[](size_t index);
+
+    bool load_from_file(const std::string& filename);
+    bool save_to_file(const std::string& filename);
+};
+
+#include "ksw_vector.cpp"
+
+#endif
